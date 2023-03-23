2ab007c97354c8a8889ba5834cdeb7e573cb1536
lep511 <estebanpbuday@gmail.com>
2023-03-23 19:22:25 -0300

    Add

---
+++ terraform/aws/dynamodb.tf
@@ -0,0 +1,49 @@
+# Example of a DynamoDB table
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/dynamodb_table
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+	# checkov:skip=CKV_AWS_119: "DynamoDB tables are not encrypted by default"
+  name           = "GameScores"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
+  point_in_time_recovery {
+    enabled = true
+  }
+}
